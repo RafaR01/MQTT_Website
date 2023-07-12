7ca26ea5c9afd4d9d2302f688ece0d181d7b29f5
RafaR01 <80701627+RafaR01@users.noreply.github.com>
2023-07-12 03:27:48 +0100

    second commit

---
+++ backend.py
@@ -0,0 +1,42 @@
+from flask import Flask
+import paho.mqtt.client as mqtt
+import os
+
+app = Flask(__name__)
+mqtt_broker = "192.168.138.237"  # Replace with your MQTT broker's URL
+mqtt_topic = "test"  # Topic to publish when the value is 1
+mqtt_username = "rafael"  # MQTT broker username
+mqtt_password = "rafael"  # MQTT broker password
+
+@app.route('/button')
+def button():
+    # Toggle the value
+    value = read_value()
+    if value == '1':
+        value = '0'
+        publish_mqtt(value)
+    else:
+        value = '1'
+        publish_mqtt(value)
+
+    # Write the updated value to the file
+    write_value(value)
+
+    return 'Button clicked'
+
+def read_value():
+    with open('value.txt', 'r') as file:
+        return file.read().strip()
+
+def write_value(value):
+    with open('value.txt', 'w') as file:
+        file.write(value)
+
+def publish_mqtt(value):
+    client = mqtt.Client()
+    client.username_pw_set(username=mqtt_username, password=mqtt_password)
+    client.connect(mqtt_broker)
+    client.publish(mqtt_topic, value)
+
+if __name__ == '__main__':
+    app.run()
